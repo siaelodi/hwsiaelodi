4b060e11ad61a14713e74c5041676ce9a600ac9d
Alex Nem <61789909+siaelodi@users.noreply.github.com>
2020-11-23 21:44:06 +0600

    //homework #2: add activities and fragments

---
+++ app/src/main/java/com/example/hwsiaelodi/MovieDetailsActivity.kt
@@ -0,0 +1,17 @@
+package com.example.hwsiaelodi
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+
+
+
+    }
+}
